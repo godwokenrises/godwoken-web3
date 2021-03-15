f628002eeb2737860a69d8c67c10c60cc2f33314
hupeng <bitrocks.hu@gmail.com>
2021-03-15 10:59:35 +0800

    chore: fmt

--- packages/api-server/knexfile.ts
+++ packages/api-server/knexfile.ts
@@ -13,38 +13,6 @@ module.exports = {
     migrations: {
       tableName: "knex_migrations"
     }
-  },
-
-  staging: {
-    client: "postgresql",
-    connection: {
-      database: "my_db",
-      user: "username",
-      password: "password"
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: "knex_migrations"
-    }
-  },
-
-  production: {
-    client: "postgresql",
-    connection: {
-      database: "my_db",
-      user: "username",
-      password: "password"
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: "knex_migrations"
-    }
   }
 
 };
