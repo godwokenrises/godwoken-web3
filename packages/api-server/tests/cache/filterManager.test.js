9ca0f4cc722710a9517af819834e51b001cbfafe
RetricSu <inneverland2013@gmail.com>
2021-03-19 12:47:57 +0800

    wip: fmt

--- packages/api-server/tests/cache/filterManager.test.js
+++ packages/api-server/tests/cache/filterManager.test.js
@@ -1,54 +1,48 @@
-const test = require('ava');
-const { FilterManager } = require('../../lib/cache/index');
+const test = require("ava");
+const { FilterManager } = require("../../lib/cache/index");
 
 const filter = new FilterManager(1000, 1000);
 const f = {
-    address: '0x0000',
-    fromBlock: 123,
-    toBlock: 'latest',
-    topics: [
-        '0x00012103230230',
-        '0x00000000000000'
-    ],
+  address: "0x0000",
+  fromBlock: 123,
+  toBlock: "latest",
+  topics: ["0x00012103230230", "0x00000000000000"],
 };
 
 const f1 = {
-    address: '0x0000',
-    fromBlock: 123,
-    toBlock: 520,
-    topics: [
-        '0x00012103230230',
-        '0x00000000000000'
-    ],
-}
+  address: "0x0000",
+  fromBlock: 123,
+  toBlock: 520,
+  topics: ["0x00012103230230", "0x00000000000000"],
+};
 
-test.serial('filter_install', t => {
-    filter.install(f);
-    filter.install(f1);
-    t.is(filter.size(), 2);
+test.serial("filter_install", (t) => {
+  filter.install(f);
+  filter.install(f1);
+  t.is(filter.size(), 2);
 });
 
-test.serial('filter_get', t => {
-    t.is(filter.get(1), f);
+test.serial("filter_get", (t) => {
+  t.is(filter.get(1), f);
 });
 
-test.serial('filter_getLastPoll', t => {
-    t.is(filter.getLastPoll(1), 0);
-})
-
-test.serial('filter_updateLastPoll', t => {
-    filter.updateLastPoll(1, 25);
-    t.is(filter.getLastPoll(1), 25);
-})
-
-test.serial('filter_uninstall', t => {
-    filter.uninstall(1);
-    t.is(filter.size(), 1);
+test.serial("filter_getLastPoll", (t) => {
+  t.is(filter.getLastPoll(1), 0);
 });
 
-test.serial.cb('filter_cache_expired', t => {
-    setTimeout(() => {
-        t.is(filter.size(), 0);
-        t.end();
-    }, 4000);
+test.serial("filter_updateLastPoll", (t) => {
+  filter.updateLastPoll(1, 25);
+  t.is(filter.getLastPoll(1), 25);
+});
+
+test.serial("filter_uninstall", (t) => {
+  filter.uninstall(1);
+  t.is(filter.size(), 1);
+});
+
+test.serial.cb("filter_cache_expired", (t) => {
+  setTimeout(() => {
+    t.is(filter.size(), 0);
+    t.end();
+  }, 4000);
 });
--- packages/api-server/tests/methods/net.test.js
+++ packages/api-server/tests/methods/net.test.js
@@ -1,26 +1,26 @@
-const test = require('ava');
-const { client } = require('../client');
+const test = require("ava");
+const { client } = require("../client");
 
-test.cb('net_version', t => {
-    return client.request('net_version', [], function(err, response) {
-        if(err) throw err;
-        t.is(response.result, '1');
-        t.end();
-    });
+test.cb("net_version", (t) => {
+  return client.request("net_version", [], function (err, response) {
+    if (err) throw err;
+    t.is(response.result, "1");
+    t.end();
+  });
 });
 
-test.cb('net_peerCount', t => {
-    return client.request('net_peerCount', [], function(err, response) {
-        if(err) throw err;
-        t.is(response.result, 0);
-        t.end();
-    });
+test.cb("net_peerCount", (t) => {
+  return client.request("net_peerCount", [], function (err, response) {
+    if (err) throw err;
+    t.is(response.result, 0);
+    t.end();
+  });
 });
 
-test.cb('net_listening', t => {
-    return client.request('net_listening', [], function(err, response) {
-        if(err) throw err;
-        t.is(response.result, true);
-        t.end();
-    });
+test.cb("net_listening", (t) => {
+  return client.request("net_listening", [], function (err, response) {
+    if (err) throw err;
+    t.is(response.result, true);
+    t.end();
+  });
 });
