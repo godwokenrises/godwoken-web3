5d19b1e12c4e3c34c4ab34d77b79eea89ad98029
retricsu <inneverland2013@gmail.com>
2021-07-21 13:42:14 +0200

    feat(poly): add more rpc methods

--- packages/api-server/src/methods/modules/poly.ts
+++ packages/api-server/src/methods/modules/poly.ts
@@ -134,4 +134,26 @@ export class Poly {
   async getDefaultFromAddress(args: [], callback: Callback) {
     callback(null, process.env.DEFAULT_FROM_ADDRESS);
   }
+
+  async getChainId(args: [], callback: Callback) {
+    try {
+      const chain_id_hex = "0x" + BigInt(process.env.CHAIN_ID!).toString(16);
+      callback(null, chain_id_hex);
+    } catch (error) {
+      callback({
+        code: WEB3_ERROR,
+        message: error.message,
+      });
+    }
+  }
+
+  async getContractValidatorTypeHash(args: [], callback: Callback) {
+    if (process.env.POLYJUICE_VALIDATOR_TYPE_HASH)
+      callback(null, process.env.POLYJUICE_VALIDATOR_TYPE_HASH!);
+    else
+      callback({
+        code: WEB3_ERROR,
+        message: "POLYJUICE_VALIDATOR_TYPE_HASH not found!",
+      });
+  }
 }
