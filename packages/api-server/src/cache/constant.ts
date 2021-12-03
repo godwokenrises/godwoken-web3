3ab1b0304178dec8c9afc3205ffb04a2eed597b2
Flouse <flouse@gmail.com>
2021-12-03 16:16:11 +0000

    yarn lint and fmt

--- packages/api-server/src/cache/constant.ts
+++ packages/api-server/src/cache/constant.ts
@@ -4,4 +4,4 @@ export const CACHE_EXPIRED_TIME_MILSECS = 5 * 60 * 1000; // milsec, default 5 mi
 export const MAX_FILTER_TOPIC_ARRAY_LENGTH = 20;
 
 // The Cache Key Prfixs
-export const GW_RPC_KEY = 'gwRPC';
+export const GW_RPC_KEY = "gwRPC";
