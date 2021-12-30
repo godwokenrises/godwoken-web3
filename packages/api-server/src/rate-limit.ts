ba12f8275be609bcf3379cfbb052a6a46c993127
RetricSu <inneverland2013@gmail.com>
2021-12-30 16:07:36 +0800

    Update packages/api-server/src/rate-limit.ts
    Co-authored-by: CL <classicalliu@gmail.com>

--- packages/api-server/src/rate-limit.ts
+++ packages/api-server/src/rate-limit.ts
@@ -50,7 +50,7 @@ export async function rateLimit(
       isBan = true;
       console.debug(`Rate Limit Exceed, ip: ${reqId}, method: ${rpcMethod}`);
 
-      const message = `Rate limit exceeded for your ip, please wait 1min and retry. Rpc method: ${rpcMethod}.`;
+      const message = `Rate limit exceeded for your ip, please wait 1min and retry. RPC method: ${rpcMethod}.`;
       const error = {
         code: LIMIT_EXCEEDED,
         message: message,
