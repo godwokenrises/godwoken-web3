faa24e29dc05143cd64c0ac3d9a84da2d462cb05
classicalliu <classicalliu@gmail.com>
2022-02-23 20:44:56 +0800

    chore: Update insufficient balance message

--- packages/api-server/src/methods/gw-error.ts
+++ packages/api-server/src/methods/gw-error.ts
@@ -42,8 +42,8 @@ export interface GwErrorItem {
 }
 const gwErrorMapping: { [key: string]: { type: string; message: string } } = {
   "92": {
-    type: "GW_SUDT_ERROR_INSUFFICIENT_BALANCE",
-    message: "insufficient balance", // TODO: update message
+    type: "SUDT_ERROR_INSUFFICIENT_BALANCE",
+    message: "sender doesn't have enough funds to send tx",
   },
 };
 
