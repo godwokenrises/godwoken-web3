557c111cf1e0fa39b10d658770e98f2f44185a17
keroro <keroroxx520@gmail.com>
2022-04-05 00:39:27 +0800

    chore: prettier fmt typescript files

--- packages/api-server/migrations/20210805143951_accounts.ts
+++ packages/api-server/migrations/20210805143951_accounts.ts
@@ -1,17 +1,16 @@
 import { Knex as KnexType } from "knex";
 
 export async function up(knex: KnexType): Promise<void> {
-  await knex.schema
-    .createTable("accounts", function (table: KnexType.TableBuilder) {
+  await knex.schema.createTable(
+    "accounts",
+    function (table: KnexType.TableBuilder) {
       table.increments();
-      table.binary("eth_address").notNullable().unique()
-      table.binary("gw_short_address").notNullable().index()
-    })
+      table.binary("eth_address").notNullable().unique();
+      table.binary("gw_short_address").notNullable().index();
+    }
+  );
 }
-
 
 export async function down(knex: KnexType): Promise<void> {
-  await knex.schema
-    .dropTable("accounts")
+  await knex.schema.dropTable("accounts");
 }
-
