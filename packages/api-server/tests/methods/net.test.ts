31ab6fdb9ec64ad43485fbb76eab28f9aefc0e0c
keroro <keroroxx520@gmail.com>
2022-04-04 20:44:41 +0800

    test: rewrite testcases using TypeScript

---
+++ packages/api-server/tests/methods/net.test.ts
@@ -0,0 +1,19 @@
+import test from "ava";
+import { client, JSONResponse } from "../www";
+
+test("net_version", async (t) => {
+  let res: JSONResponse = await client.request(t.title, []);
+  t.falsy(res.error);
+});
+
+test("net_peerCount", async (t) => {
+  let res: JSONResponse = await client.request(t.title, []);
+  t.falsy(res.error);
+  t.is(res.result, "0x0");
+});
+
+test("net_listening", async (t) => {
+  let res: JSONResponse = await client.request(t.title, []);
+  t.falsy(res.error);
+  t.is(res.result, true);
+});
