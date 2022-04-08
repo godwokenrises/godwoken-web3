7e16d58e03137b20daf3571ccfc190cad538097f
RetricSu <inneverland2013@gmail.com>
2022-04-08 15:07:06 +0800

    refactor(validator): verify params with correct types and full context

--- packages/api-server/src/methods/validator.ts
+++ packages/api-server/src/methods/validator.ts
@@ -83,17 +83,7 @@ export const validators = {
    * @param {number} index index of parameter
    */
   blockHash(params: any[], index: number): any {
-    if (typeof params[index] !== "string") {
-      return invalidParamsError(index, `argument must be a hex string`);
-    }
-
-    const blockHash = params[index].substring(2);
-
-    if (!/^[0-9a-fA-F]+$/.test(blockHash) || blockHash.length !== 64) {
-      return invalidParamsError(index, `invalid block hash`);
-    }
-
-    return undefined;
+    return verifyBlockHash(params[index], index);
   },
 
   /**
@@ -102,17 +92,7 @@ export const validators = {
    * @param {number} index index of parameter
    */
   txHash(params: any[], index: number): any {
-    if (typeof params[index] !== "string") {
-      return invalidParamsError(index, `argument must be a hex string`);
-    }
-
-    const txHash = params[index].substring(2);
-
-    if (!/^[0-9a-fA-F]+$/.test(txHash) || txHash.length !== 64) {
-      return invalidParamsError(index, `invalid transaction hash`);
-    }
-
-    return undefined;
+    return verifyTxHash(params[index], index);
   },
 
   /**
@@ -130,258 +110,152 @@ export const validators = {
    * @param {number} index index of parameter
    */
   bool(params: any[], index: number): any {
-    if (typeof params[index] !== "boolean") {
-      return invalidParamsError(index, `argument is not boolean`);
-    }
-    return undefined;
+    return verifyBoolean(params[index], index);
   },
 
   ethCallParams(params: any[], index: number): any {
-    const targetParam = params[index];
-    if (typeof targetParam !== "object") {
-      return invalidParamsError(index, `argument must be an object`);
-    }
-
-    const from = targetParam.from;
-    const to = targetParam.to;
-    const gas = targetParam.gas;
-    const gasLimit = targetParam.gasLimit;
-    const value = targetParam.value;
-    const data = targetParam.data;
-
-    // to is required
-    if (to == null) {
-      return invalidParamsError(index, `to address is null!`);
-    }
-
-    // validate `to`
-    if (to !== undefined && to !== null) {
-      const toErr = verifyAddress(to, index);
-      if (toErr !== undefined) {
-        return toErr;
-      }
-    }
-
-    // validate `from`
-    if (from !== undefined && from !== null) {
-      const fromErr = verifyAddress(from, index);
-      if (fromErr !== undefined) {
-        return fromErr;
-      }
-    }
-
-    // validate `gas`
-    if (gas !== undefined && gas !== null) {
-      const gasErr = verifyHexNumber(gas, index);
-      if (gasErr !== undefined) {
-        return gasErr;
-      }
-    }
-
-    // validate `gasLimit`
-    if (gasLimit !== undefined && gasLimit !== null) {
-      const gasLimitErr = verifyHexNumber(gasLimit, index);
-      if (gasLimitErr !== undefined) {
-        return gasLimitErr;
-      }
-    }
-
-    // validate `value`
-    if (value !== undefined && value !== null) {
-      const valueErr = verifyHexNumber(value, index);
-      if (valueErr !== undefined) {
-        return valueErr;
-      }
-    }
-
-    // validate `data`
-    if (data !== undefined && data !== null) {
-      const dataErr = verifyHexString(data, index);
-      if (dataErr !== undefined) {
-        return dataErr;
-      }
-    }
-
-    return undefined;
+    return verifyEthCallObject(params[index], index);
   },
 
   ethEstimateGasParams(params: any[], index: number): any {
-    const targetParam = params[index];
-    if (typeof targetParam !== "object") {
-      return invalidParamsError(index, `argument must be an object`);
-    }
-
-    const from = targetParam.from;
-    const to = targetParam.to;
-    const gas = targetParam.gas;
-    const gasLimit = targetParam.gasLimit;
-    const value = targetParam.value;
-    const data = targetParam.data;
-
-    // validate `to`
-    if (to !== undefined && to !== null) {
-      const toErr = verifyAddress(to, index);
-      if (toErr !== undefined) {
-        return toErr;
-      }
-    }
-
-    // validate `from`
-    if (from !== undefined && from !== null) {
-      const fromErr = verifyAddress(from, index);
-      if (fromErr !== undefined) {
-        return fromErr;
-      }
-    }
-
-    // validate `gas`
-    if (gas !== undefined && gas !== null) {
-      const gasErr = verifyHexNumber(gas, index);
-      if (gasErr !== undefined) {
-        return gasErr;
-      }
-    }
-
-    // validate `gasLimit`
-    if (gasLimit !== undefined && gasLimit !== null) {
-      const gasLimitErr = verifyHexNumber(gasLimit, index);
-      if (gasLimitErr !== undefined) {
-        return gasLimitErr;
-      }
-    }
-
-    // validate `value`
-    if (value !== undefined && value !== null) {
-      const valueErr = verifyHexNumber(value, index);
-      if (valueErr !== undefined) {
-        return valueErr;
-      }
-    }
-
-    // validate `data`
-    if (data !== undefined && data !== null) {
-      const dataErr = verifyHexString(data, index);
-      if (dataErr !== undefined) {
-        return dataErr;
-      }
-    }
-
-    return undefined;
+    return verifyEstimateGasCallObject(params[index], index);
   },
 
   newFilterParams(params: any[], index: number): any {
-    const targetParam = params[index];
-    if (typeof targetParam !== "object") {
-      return invalidParamsError(index, `argument must be an object`);
-    }
-
-    const fromBlock = targetParam.fromBlock;
-    const toBlock = targetParam.toBlock;
-    const address = targetParam.address;
-    const topics = targetParam.topics;
-
-    // validate `fromBlock`
-    if (fromBlock !== undefined && fromBlock !== null) {
-      const fromBlockErr = verifyBlockParameter(fromBlock, index);
-      if (fromBlockErr !== undefined) {
-        return fromBlockErr;
-      }
-    }
-
-    // validate `toBlock`
-    if (toBlock !== undefined && toBlock !== null) {
-      const toBlockErr = verifyBlockParameter(toBlock, index);
-      if (toBlockErr !== undefined) {
-        return toBlockErr;
-      }
-    }
-
-    // validate `address`
-    if (address !== undefined && address !== null) {
-      if (Array.isArray(address)) {
-        for (const addr of address) {
-          const addressErr = verifyAddress(addr, index);
-          if (addressErr !== undefined) {
-            return addressErr;
-          }
-        }
-      } else {
-        const addressErr = verifyAddress(address, index);
-        if (addressErr !== undefined) {
-          return addressErr;
-        }
-      }
-    }
-
-    // validate `topics`
-    if (topics !== undefined && topics !== null) {
-      if (!Array.isArray(topics)) {
-        return invalidParamsError(index, `argument must be an array`);
-      }
-      for (const topic of topics) {
-        verifyFilterTopic(topic, index);
-      }
-    }
-
-    return undefined;
+    return verifyNewFilterObj(params[index], index);
   },
 };
 
-function verifyAddress(address: any, index: number): any {
-  if (typeof address !== "string") {
-    return invalidParamsError(index, `argument must be a hex string`);
+//****** standalone verify function ********/
+export function verifyBoolean(
+  bool: any,
+  index: number
+): InvalidParamsError | undefined {
+  if (typeof bool !== "boolean") {
+    return invalidParamsError(index, `argument is not boolean`);
   }
+  return undefined;
+}
 
-  if (!validateAddress(address)) {
+export function verifyHexNumber(
+  hexNumber: string,
+  index: number
+): InvalidParamsError | undefined {
+  if (typeof hexNumber !== "string") {
     return invalidParamsError(
       index,
-      `address must be a 20 bytes-length hex string`
+      `hexNumber argument must be a string type`
+    );
+  }
+
+  if (!hexNumber.startsWith("0x")) {
+    return invalidParamsError(index, `hexNumber without 0x prefix`);
+  }
+
+  if (hexNumber.startsWith("0x0") && hexNumber !== "0x0") {
+    return invalidParamsError(index, `hexNumber with leading zero digits`);
+  }
+
+  if (!validateHexNumber(hexNumber)) {
+    return invalidParamsError(index, `invalid hexNumber token`);
+  }
+
+  return undefined;
+}
+
+export function verifyHexString(
+  hexString: any,
+  index: number
+): InvalidParamsError | undefined {
+  if (typeof hexString !== "string") {
+    return invalidParamsError(
+      index,
+      `hexString argument must be a string type`
+    );
+  }
+
+  if (!hexString.startsWith("0x")) {
+    return invalidParamsError(index, `hexString without 0x prefix`);
+  }
+
+  if (hexString.length % 2 !== 0) {
+    return invalidParamsError(index, `hexString must has even length`);
+  }
+
+  if (!validateHexString(hexString)) {
+    return invalidParamsError(index, `invalid hexString token`);
+  }
+
+  return undefined;
+}
+
+export function verifyAddress(
+  address: any,
+  index: number
+): InvalidParamsError | undefined {
+  const err = verifyHexString(address, index);
+  if (err) {
+    return padErrorContext(err, "address");
+  }
+
+  if (address.substring(2).length !== 40) {
+    return invalidParamsError(
+      index,
+      `expect address has 20 bytes, but getting ${
+        address.substring(2).length / 2
+      } bytes`
     );
   }
 
   return undefined;
 }
 
-function verifyHexNumber(hexNumber: string, index: number) {
-  if (typeof hexNumber !== "string") {
-    return invalidParamsError(index, `argument must be a hex string`);
+export function verifyBlockHash(
+  blockHash: any,
+  index: number
+): InvalidParamsError | undefined {
+  const err = verifyHexString(blockHash, index);
+  if (err) {
+    return padErrorContext(err, "blockHash");
   }
 
-  if (!hexNumber.startsWith("0x")) {
-    return invalidParamsError(index, `hex string without 0x prefix`);
-  }
-
-  if (hexNumber.startsWith("0x0") && hexNumber !== "0x0") {
-    return invalidParamsError(index, `hex number with leading zero digits`);
-  }
-
-  if (!validateHexNumber(hexNumber)) {
-    return invalidParamsError(index, `invalid hex number`);
-  }
-
-  return undefined;
-}
-
-function verifyStorageKey(key: string, index: number) {
-  if (typeof key !== "string") {
-    return invalidParamsError(index, `argument must be a hex string`);
-  }
-
-  if (!key.startsWith("0x")) {
-    return invalidParamsError(index, `hex string without 0x prefix`);
-  }
-
-  if (!/^0x([0-9a-fA-F]*)$/.test(key)) {
-    return invalidParamsError(index, `invalid hex`);
+  if (blockHash.substring(2).length !== 64) {
+    return invalidParamsError(
+      index,
+      `expect blockHash has 32 bytes, but getting ${
+        blockHash.substring(2).length / 2
+      } bytes`
+    );
   }
 
   return undefined;
 }
 
-function verifyBlockParameter(
+export function verifyTxHash(
+  txHash: any,
+  index: number
+): InvalidParamsError | undefined {
+  const err = verifyHexString(txHash, index);
+  if (err) {
+    return padErrorContext(err, "txHash");
+  }
+
+  if (txHash.substring(2).length !== 64) {
+    return invalidParamsError(
+      index,
+      `expect txHash has 32 bytes, but getting ${
+        txHash.substring(2).length / 2
+      } bytes`
+    );
+  }
+
+  return undefined;
+}
+
+export function verifyBlockParameter(
   blockParameter: BlockParameter,
   index: number
-): any {
+): InvalidParamsError | undefined {
   if (
     blockParameter === "latest" ||
     blockParameter === "earliest" ||
@@ -390,44 +264,155 @@ function verifyBlockParameter(
     return undefined;
   }
 
-  if (typeof blockParameter !== "string") {
-    return invalidParamsError(
-      index,
-      `argument must be a hex number, "latest", "earliest" or "pending".`
-    );
-  }
-
-  return verifyHexNumber(blockParameter, index);
-}
-
-function verifyHexString(hexString: any, index: number): any {
-  if (typeof hexString !== "string") {
-    return invalidParamsError(index, `argument must be a hex string`);
-  }
-
-  if (!hexString.startsWith("0x")) {
-    return invalidParamsError(index, `hex string without 0x prefix`);
-  }
-
-  if (!validateHexString(hexString)) {
-    return invalidParamsError(index, `invalid hex string`);
+  const err = verifyHexNumber(blockParameter, index);
+  if (err) {
+    return padErrorContext(err, "blockParameter block number");
   }
 
   return undefined;
 }
 
-function verifyFilterTopic(topic: any, index: number): any {
+export function verifyOptEthCallObject(
+  callObj: any,
+  index: number
+): InvalidParamsError | undefined {
+  if (typeof callObj !== "object") {
+    return invalidParamsError(index, `argument must be an object`);
+  }
+
+  const from = callObj.from;
+  const to = callObj.to;
+  const gas = callObj.gas;
+  const gasLimit = callObj.gasLimit;
+  const value = callObj.value;
+  const data = callObj.data;
+
+  // validate `to`
+  if (to != null) {
+    const toErr = verifyAddress(to, index);
+    if (toErr) {
+      return padErrorContext(toErr, "callObj to address");
+    }
+  }
+
+  // validate `from`
+  if (from != null) {
+    const fromErr = verifyAddress(from, index);
+    if (fromErr) {
+      return padErrorContext(fromErr, "callObj from address");
+    }
+  }
+
+  // validate `gas`
+  if (gas != null) {
+    const gasErr = verifyHexNumber(gas, index);
+    if (gasErr) {
+      return padErrorContext(gasErr, "callObj gas");
+    }
+  }
+
+  // validate `gasLimit`
+  if (gasLimit != null) {
+    const gasLimitErr = verifyHexNumber(gasLimit, index);
+    if (gasLimitErr) {
+      return padErrorContext(gasLimitErr, "callObj gasLimit");
+    }
+  }
+
+  // validate `value`
+  if (value != null) {
+    const valueErr = verifyHexNumber(value, index);
+    if (valueErr) {
+      return padErrorContext(valueErr, "callObj value");
+    }
+  }
+
+  // validate `data`
+  if (data != null) {
+    const dataErr = verifyHexString(data, index);
+    if (dataErr) {
+      return padErrorContext(dataErr, "callObj data");
+    }
+  }
+
+  return undefined;
+}
+
+export function verifyEthCallObject(
+  callObj: any,
+  index: number
+): InvalidParamsError | undefined {
+  const err = verifyOptEthCallObject(callObj, index);
+  if (err) {
+    return padErrorContext(err, "eth_call");
+  }
+
+  // to is required
+  if (callObj.to == null) {
+    return invalidParamsError(index, `eth_call to address is required!`);
+  }
+
+  return undefined;
+}
+
+export function verifyEstimateGasCallObject(
+  callObj: any,
+  index: number
+): InvalidParamsError | undefined {
+  const err = verifyOptEthCallObject(callObj, index);
+  if (err) {
+    return padErrorContext(err, "eth_estimateGas");
+  }
+
+  return undefined;
+}
+
+export function verifyStorageKey(
+  key: string,
+  index: number
+): InvalidParamsError | undefined {
+  const err = verifyHexString(key, index);
+  if (err) {
+    return padErrorContext(err, "storageKey");
+  }
+  return undefined;
+}
+
+export function verifyFilterTopicString(
+  topic: any,
+  index: number
+): InvalidParamsError | undefined {
+  const err = verifyHexString(topic, index);
+  if (err) {
+    return padErrorContext(err, "topic string");
+  }
+
+  if (topic.substring(2).length !== 64) {
+    return invalidParamsError(
+      index,
+      `expect topic string has 32 bytes, but getting ${
+        topic.substring(2).length / 2
+      } bytes`
+    );
+  }
+
+  return undefined;
+}
+
+export function verifyFilterTopic(
+  topic: any,
+  index: number
+): InvalidParamsError | undefined {
   // topic type: export type FilterTopic = HexString | null | HexString[] (../src/cache/type.ts)
   if (!Array.isArray(topic)) {
-    if ((!validateHexString(topic) || topic.length !== 66) && topic !== null) {
-      return invalidParamsError(index, `invalid argument`);
-    }
+    return verifyFilterTopicString(topic, index);
   }
 
   if (Array.isArray(topic)) {
     for (const t of topic) {
-      if ((!validateHexString(t) || t.length !== 66) && t !== null) {
-        return invalidParamsError(index, `invalid argument`);
+      const err = verifyFilterTopicString(t, index);
+      if (err) {
+        return padErrorContext(err, "topicString[] array");
       }
     }
   }
@@ -435,10 +420,88 @@ function verifyFilterTopic(topic: any, index: number): any {
   return undefined;
 }
 
-function validateAddress(address: string): boolean {
-  return /^0x[0-9a-fA-F]+$/.test(address) && address.length === 42;
-}
+export function verifyNewFilterObj(
+  filterObj: any,
+  index: number
+): InvalidParamsError | undefined {
+  if (typeof filterObj !== "object") {
+    return invalidParamsError(index, `filter argument must be an object`);
+  }
 
+  const fromBlock = filterObj.fromBlock;
+  const toBlock = filterObj.toBlock;
+  const address = filterObj.address;
+  const topics = filterObj.topics;
+
+  // validate `fromBlock`
+  if (fromBlock != null) {
+    const fromBlockErr = verifyBlockParameter(fromBlock, index);
+    if (fromBlockErr) {
+      return padErrorContext(fromBlockErr, "filter fromBlock");
+    }
+  }
+
+  // validate `toBlock`
+  if (toBlock != null) {
+    const toBlockErr = verifyBlockParameter(toBlock, index);
+    if (toBlockErr) {
+      return padErrorContext(toBlockErr, "filter toBlock");
+    }
+  }
+
+  // validate `address`
+  if (address != null) {
+    if (Array.isArray(address)) {
+      for (const addr of address) {
+        const addressErr = verifyAddress(addr, index);
+        if (addressErr) {
+          return padErrorContext(addressErr, "filter address[] element");
+        }
+      }
+    } else {
+      const addressErr = verifyAddress(address, index);
+      if (addressErr) {
+        return padErrorContext(addressErr, "filter address");
+      }
+    }
+  }
+
+  // validate `topics`
+  if (topics != null) {
+    if (!Array.isArray(topics)) {
+      return invalidParamsError(index, `filter topics must be an array`);
+    }
+    for (const topic of topics) {
+      const topicErr = verifyFilterTopic(topic, index);
+      if (topicErr) {
+        return padErrorContext(topicErr, "filter topic[] Array");
+      }
+    }
+  }
+
+  return undefined;
+}
+//******* end of standalone verify function ********/
+
+// some utils function
 function invalidParamsError(index: number, message: string): any {
   return new InvalidParamsError(`invalid argument ${index}: ${message}`);
+}
+
+function padErrorContext(
+  err: InvalidParamsError,
+  context: string
+): InvalidParamsError {
+  const msgs = err.message.split(/(invalid argument .: )/);
+  // [ '', 'invalid argument <number>: ', 'message' ]
+  if (msgs.length !== 3) {
+    throw new Error(
+      `parse InvalidParamsError message failed: ${err.message}, ${msgs}`
+    );
+  }
+  const newMsg = `${msgs[1]}${context} -> ${msgs[2]}`;
+  return {
+    ...err,
+    ...{ message: newMsg },
+  } as InvalidParamsError;
 }
