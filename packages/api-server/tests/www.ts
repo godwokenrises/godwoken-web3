f36f1e86fd1c45d582e60e79b6fd07297b6a8f41
keroro <keroroxx520@gmail.com>
2022-05-09 11:01:31 +0800

    test: create workflow for godwoken-tests
    Why: Reuse godwoken-tests's testing workflow to enforce Godwoken-Web3.
    
    How: `workflow_call` nervosnetwork/godwoken-tests/.github/workflows/reusable-integration-test-v1.yml

--- packages/api-server/tests/www.ts
+++ packages/api-server/tests/www.ts
@@ -1,7 +1,7 @@
 import jayson from "jayson/promise";
 
 export const client = jayson.Client.http({
-  port: process.env.PORT == null ? "8024" : process.env.PORT,
+  port: process.env.PORT || "8024",
 });
 
 export interface JSONResponse {
