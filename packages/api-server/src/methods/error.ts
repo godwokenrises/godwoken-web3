7e33b922c4aa330d51bfd2a198b6e65d0766bd87
keroro <keroroxx520@gmail.com>
2022-05-23 12:05:35 +0800

    feat: add an unified error type AppError

--- packages/api-server/src/methods/error.ts
+++ packages/api-server/src/methods/error.ts
@@ -1,76 +1,160 @@
-import { JSONRPCError } from "jayson";
-import { logger } from "../base/logger";
-import { HEADER_NOT_FOUND_ERR_MESSAGE } from "./constant";
-import {
-  HEADER_NOT_FOUND_ERROR,
-  INTERNAL_ERROR,
-  INVALID_PARAMS,
-  LIMIT_EXCEEDED,
-  METHOD_NOT_SUPPORT,
-  WEB3_ERROR,
-} from "./error-code";
+//  Error code from JSON-RPC 2.0 spec
+//  reference: http://www.jsonrpc.org/specification#error_object
 
-export class RpcError extends Error implements JSONRPCError {
+import { JSONRPCError } from "jayson";
+import { COMPATIBLE_DOCS_URL } from "./constant";
+
+export interface ErrorKind {
+  code: number;
+  message: string;
+}
+
+export class AppError extends Error implements JSONRPCError {
   code: number;
   data?: object;
 
-  constructor(code: number, message: string, data?: object) {
-    super(message);
-    this.name = "RpcError";
-
-    this.code = code;
+  constructor(kind: ErrorKind, data?: object) {
+    super(kind.message);
+    this.name = kind.message;
+    this.code = kind.code;
     this.data = data;
   }
-}
 
-export class Web3Error extends RpcError {
-  constructor(message: string, data?: object) {
-    super(WEB3_ERROR, message, data);
-  }
-}
-
-export class InvalidParamsError extends RpcError {
-  constructor(message: string) {
-    super(INVALID_PARAMS, message);
-  }
-
-  padContext(context: string): InvalidParamsError {
-    const msgs = this.message.split(/(invalid argument .: )/);
-    // [ '', 'invalid argument <number>: ', 'message' ]
-    if (msgs.length !== 3) {
-      logger.error(
-        `[InvalidParamsError] padContext parse message failed: ${
-          this.message
-        }, split array: ${JSON.stringify(msgs)}, will return origin error.`
-      );
-      return this;
-    }
-    const newMsg = `${msgs[1]}${context} -> ${msgs[2]}`;
-    this.message = newMsg;
+  addContext(context: Object): AppError {
+    this.data = {
+      ...this.data,
+      ...context,
+    };
     return this;
   }
 }
 
-export class InternalError extends RpcError {
-  constructor(message: string) {
-    super(INTERNAL_ERROR, message);
-  }
-}
+const DEFAULT_ERROR_CODE: number = -32000;
 
-export class MethodNotSupportError extends RpcError {
-  constructor(message: string) {
-    super(METHOD_NOT_SUPPORT, message);
-  }
-}
+export const ERRORS = Object.freeze({
+  INVALID_PARAMETER: {
+    code: -32602,
+    message: "invalid parameter",
+  } as ErrorKind,
+  MISSING_PARAMETER: {
+    code: DEFAULT_ERROR_CODE,
+    message: "missing parameter",
+  } as ErrorKind,
+  JSONRPC_METHOD_NOT_SUPPORTED: {
+    code: -32004,
+    message: "JSONRPC method not supported",
+  } as ErrorKind,
 
-export class HeaderNotFoundError extends RpcError {
-  constructor(message: string = HEADER_NOT_FOUND_ERR_MESSAGE) {
-    super(HEADER_NOT_FOUND_ERROR, message);
-  }
-}
+  RATE_LIMITED: {
+    code: -32005,
+    message: "rate limited",
+  } as ErrorKind,
 
-export class LimitExceedError extends RpcError {
-  constructor(message: string) {
-    super(LIMIT_EXCEEDED, message);
-  }
-}
+  INTERNAL_ERROR: {
+    code: -32603,
+    message: "internal error",
+  } as ErrorKind,
+
+  GW_ERROR: {
+    code: -32098,
+    message: "Godwoken JSONRPC error",
+  } as ErrorKind,
+  BLOCK_NOT_FOUND: {
+    code: DEFAULT_ERROR_CODE,
+    message: "block not found",
+  } as ErrorKind,
+
+  CONNECTION_RESET: {
+    code: DEFAULT_ERROR_CODE,
+    message: "connection reset",
+  } as ErrorKind,
+
+  DATABASE_QUERY_TOO_MANY_RESULTS: {
+    code: DEFAULT_ERROR_CODE,
+    message: "database query too many results",
+  } as ErrorKind,
+  DATABASE_QUERY_TIMEOUT: {
+    code: DEFAULT_ERROR_CODE,
+    message: "database query timeout",
+  } as ErrorKind,
+  DATABASE_QUERY_ERROR: {
+    code: DEFAULT_ERROR_CODE,
+    message: "database query error",
+  } as ErrorKind,
+  DATABASE_GENESIS_NOT_FOUND: {
+    code: DEFAULT_ERROR_CODE,
+    message: "database genesis block not found",
+  } as ErrorKind,
+  DATABASE_QUERY_OFFSET_REACHED_END: {
+    code: DEFAULT_ERROR_CODE,
+    message: "database query offset reached end",
+  } as ErrorKind,
+
+  GAS_LIMIT_TOO_LARGE: {
+    code: DEFAULT_ERROR_CODE,
+    message: "transaction gasLimit is too large",
+  } as ErrorKind,
+  TRANSACTION_DATA_TOO_LARGE: {
+    code: DEFAULT_ERROR_CODE,
+    message: "transaction data is too large",
+  } as ErrorKind,
+  FILTER_NOT_REGISTERED: {
+    code: DEFAULT_ERROR_CODE,
+    message: "filter is not registered, outdated, or removed",
+  } as ErrorKind,
+  FILTER_TOO_MANY_TOPICS: {
+    code: DEFAULT_ERROR_CODE,
+    message: "filter too many topics",
+  } as ErrorKind,
+  FILTER_TOPIC_TOO_LENGTHY: {
+    code: DEFAULT_ERROR_CODE,
+    message: "filter topic too lengthy",
+  } as ErrorKind,
+  FILTER_FLAG_NOT_SUPPORTED: {
+    code: DEFAULT_ERROR_CODE,
+    message: "filter flag not supported",
+  } as ErrorKind,
+
+  TRANSACTION_TYPE_NOT_SUPPORTED: {
+    code: DEFAULT_ERROR_CODE,
+    message: `unsupported transaction type (only EIP155 transaction is supported, see also ${COMPATIBLE_DOCS_URL}`,
+  } as ErrorKind,
+  ZERO_ADDRESS_NOT_REGISTERED: {
+    code: DEFAULT_ERROR_CODE,
+    message: `zero address is not registered (see also ${COMPATIBLE_DOCS_URL})`,
+  } as ErrorKind,
+  FROM_ADDRESS_NOT_REGISTERED: {
+    code: DEFAULT_ERROR_CODE,
+    message: `from address is not registered (see also ${COMPATIBLE_DOCS_URL})`,
+  } as ErrorKind,
+  TO_ADDRESS_NOT_REGISTERED: {
+    code: DEFAULT_ERROR_CODE,
+    message: `to address is not registered (see also ${COMPATIBLE_DOCS_URL})`,
+  } as ErrorKind,
+  TO_ADDRESS_EOA_ACCOUNT: {
+    code: DEFAULT_ERROR_CODE,
+    message: `to address is EOA account (see also ${COMPATIBLE_DOCS_URL})`,
+  } as ErrorKind,
+  INVALID_ADDRESS_FORMAT: {
+    code: DEFAULT_ERROR_CODE,
+    message: "invalid address format",
+  } as ErrorKind,
+  INVALID_ETH_REGISTRY_ADDRESS_FORMAT: {
+    code: DEFAULT_ERROR_CODE,
+    message: "invalid ETH registry address format",
+  } as ErrorKind,
+
+  REDIS_ERROR: {
+    code: DEFAULT_ERROR_CODE,
+    message: "redis error",
+  } as ErrorKind,
+  REDIS_POLL_TIMEOUT: {
+    code: DEFAULT_ERROR_CODE,
+    message: "redis pull data timeout",
+  } as ErrorKind,
+
+  UNKNOWN: {
+    code: DEFAULT_ERROR_CODE,
+    message: "unknown error",
+  } as ErrorKind,
+});
