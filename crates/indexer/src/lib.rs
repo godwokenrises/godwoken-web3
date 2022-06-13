7f294c354b10a223d747d8d639d74b7e3ffd5f15
classicalliu <classicalliu@gmail.com>
2022-06-13 13:07:53 +0800

    feat(indexer): Remove error tx receipt

--- crates/indexer/src/lib.rs
+++ crates/indexer/src/lib.rs
@@ -1,12 +1,8 @@
 pub mod config;
-pub mod error_receipt_indexer;
 pub mod helper;
 pub mod indexer;
 pub mod pool;
 pub mod runner;
 pub mod types;
-pub mod ws_client;
-pub mod ws_output;
 
-pub use error_receipt_indexer::ErrorReceiptIndexer;
 pub use indexer::Web3Indexer;
