fdfc93812bc1e1766d2e6dbbdb8119af84003e3b
Daniel Kmak <neverkuzi@gmail.com>
2022-06-24 17:49:05 +0200

    fix: fix formatting

--- packages/api-server/src/db/constant.ts
+++ packages/api-server/src/db/constant.ts
@@ -1,5 +1,8 @@
 import { envConfig } from "../base/env-config";
 
 export const MAX_QUERY_NUMBER = parseInt(envConfig.maxQueryNumber, 10);
-export const MAX_QUERY_TIME_MILSECS = parseInt(envConfig.maxQueryTimeInMilliseconds, 10);
+export const MAX_QUERY_TIME_MILSECS = parseInt(
+  envConfig.maxQueryTimeInMilliseconds,
+  10
+);
 export const MAX_QUERY_ROUNDS = parseInt(envConfig.maxQueryRounds, 10);
