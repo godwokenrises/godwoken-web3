2dcaa4e69188efb011379dc13f9adc550648ef72
RetricSu <inneverland2013@gmail.com>
2022-07-12 11:37:54 +0800

    chore: tune latest median tx to 50

--- packages/api-server/src/db/constant.ts
+++ packages/api-server/src/db/constant.ts
@@ -2,3 +2,5 @@ export const MAX_QUERY_NUMBER = 10000;
 export const MAX_QUERY_TIME_MILSECS = 1000 * 10; // 10 seconds
 
 export const MAX_QUERY_ROUNDS = 100;
+
+export const LATEST_MEDIAN_GAS_PRICE = 50;
