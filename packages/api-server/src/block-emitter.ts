7434b713601c1b6b9dad78632f5fa9187942ca41
keroro <keroroxx520@gmail.com>
2022-07-19 17:31:39 +0800

    chore: enable newrelic always

--- packages/api-server/src/block-emitter.ts
+++ packages/api-server/src/block-emitter.ts
@@ -6,10 +6,7 @@ import { toApiNewHead } from "./db/types";
 import cluster from "cluster";
 import * as Sentry from "@sentry/node";
 
-let newrelic: any = undefined;
-if (envConfig.newRelicLicenseKey) {
-  newrelic = require("newrelic");
-}
+const newrelic = require("newrelic");
 
 // Only start in main worker, and `startWorker` in workers to get emitter.
 export class BlockEmitter {
@@ -129,23 +126,19 @@ export class BlockEmitter {
     };
 
     // add new relic background transaction
-    if (envConfig.newRelicLicenseKey) {
-      return newrelic.startBackgroundTransaction(
-        `BlockEmitter#pool`,
-        async () => {
-          newrelic.getTransaction();
-          try {
-            return await executePoll();
-          } catch (error) {
-            throw error;
-          } finally {
-            newrelic.endTransaction();
-          }
+    return newrelic.startBackgroundTransaction(
+      `BlockEmitter#pool`,
+      async () => {
+        newrelic.getTransaction();
+        try {
+          return await executePoll();
+        } catch (error) {
+          throw error;
+        } finally {
+          newrelic.endTransaction();
         }
-      );
-    }
-
-    return await executePoll();
+      }
+    );
   }
 
   getEmitter(): EventEmitter {
